326e7ac7549a5cdef7d6afd24fe98ac6a5140f3f
eric-ce <eric.li@cognitive.earth>
2024-06-05 15:51:21 +1000

    feat: initialize clean sandbox

--- src/scripts/app.js
+++
@@ -1,8 +0,0 @@
-class HelloWorld extends HTMLElement {
-    constructor() {
-        super();
-        this.innerHTML = "<h1>Hello World</h1>";
-    }
-}
-
-customElements.define("hello-world", HelloWorld);
